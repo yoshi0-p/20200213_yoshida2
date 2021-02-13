866ff1c15e7707bae53219d53ab7a7c065f723ba
yoshi0-p <77954369+yoshi0-p@users.noreply.github.com>
2021-02-13 11:21:36 +0900

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,25 @@
   <div id="app">
     <input type="text" v-model="getText">
     <button @click="showAdress" type="submit">変換 </button>
-    <Header :parentText="getText"> </Header>
      <p>住所：{{show}}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+ let getText;
 export default {
+  
   data(){
     return{
-      show:""
+      show:"",
+      getText:""
     };
   },
-  props:["getText"],
-   method:{
+   methods:{
      async showAdress(){
-      let item = await axios.get('https://apis.postcode-jp.com/api/v3/postcodes/${getText}?Alladress');
-      this.show = item.data;
+      let item = await axios.get(`https://apis.postcode-jp.com/api/v3/postcodes/postcode=${getText}?apikey=fabCn3nwAAosqqjhmKDAC5fhlZKgNh6fPvWwBZY`);
+      this.show = item.allAddress;
     }
   },
 };
